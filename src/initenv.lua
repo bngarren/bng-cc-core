5573c2ca9c9c76bc293cfe371ef2716bfac282ac
Ben Garren <bngarren@gmail.com>
2025-01-29 11:42:35 -0500

    rename initenv

--- src/initenv.lua
+++ src/initenv.lua
@@ -3,6 +3,13 @@ return {
     init_env = function()
         local _require, _env = require("cc.require"), setmetatable({}, { __index = _ENV })
         require, package = _require.make(_env, "/")
+
+        -- Add custom module paths
+        local module_folder = "/bng/common/?.lua;/bng/programs/?.lua"
+        package.path = module_folder .. ";" .. package.path
+
         term.clear(); term.setCursorPos(1, 1)
+
+        print("Updated package.path: \n" .. package.path)
     end
 }
